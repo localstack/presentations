8cdfb32015e58e52932855617606e029b63438f8
Giovanni Grano <me@giograno.com>
2022-12-24 16:47:31 +0100

    Improving on the ML sample (#1)

--- 2022-08-24-Pods-BlogPost/sample_ml-model/run.sh
+++ 2022-08-24-Pods-BlogPost/sample_ml-model/run.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 zip lambda.zip train.py
+zip infer.zip infer.py
 
 # push training data to S3
 awslocal s3 mb s3://pods-test
 awslocal s3 cp lambda.zip s3://pods-test/lambda.zip
+awslocal s3 cp infer.zip s3://pods-test/infer.zip
 awslocal s3 cp digits.rst s3://pods-test/digits.rst
 awslocal s3 cp digits.csv.gz s3://pods-test/digits.csv.gz
 
 # define lamba function to training the ML data
-awslocal lambda create-function --function-name func1 \
+awslocal lambda create-function --function-name ml-train \
   --runtime python3.8 --role r1 --handler train.handler --timeout 600 \
    --code '{"S3Bucket":"pods-test","S3Key":"lambda.zip"}' \
-  --layers arn:aws:lambda:us-east-1:446751924810:layer:python-3-8-scikit-learn-0-23-1:2
+   --layers arn:aws:lambda:us-east-1:446751924810:layer:python-3-8-scikit-learn-0-22-0:3
+
+awslocal lambda create-function --function-name ml-predict \
+  --runtime python3.8 --role r1 --handler infer.handler --timeout 600 \
+   --code '{"S3Bucket":"pods-test","S3Key":"infer.zip"}' \
+   --layers arn:aws:lambda:us-east-1:446751924810:layer:python-3-8-scikit-learn-0-22-0:3
 
 # invoke the lambda function
-awslocal lambda invoke --function-name func1 /tmp/test.tmp
+awslocal lambda invoke --function-name ml-train test.tmp
 
-# create and push a Cloud Pod
-localstack pod push --name ml-pod
+awslocal lambda invoke --function-name ml-predict test.tmp
+
+# create a Cloud Pod and make it public
+localstack pod save ml-pod
+localstack pod save ml-pod --visibility public
